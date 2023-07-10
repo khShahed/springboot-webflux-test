6a4fa8207eca8c312b3f911303d1137f64643165
MD Kamrul Hasan Shahed <khshahed90@gmail.com>
2023-07-10 22:53:11 +0600

    initial commit

---
+++ src/main/java/com/example/springbootwebfluxtest/SpringbootWebfluxTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebfluxtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebfluxTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebfluxTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootwebfluxtest/SpringbootWebfluxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebfluxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
